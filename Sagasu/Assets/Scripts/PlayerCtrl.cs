5af95e882b874a75429b7d6236a82f22d29954b7
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-10-14 21:02:37 +0900

    キー操作

---
+++ Sagasu/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Character character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<Character>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyCtrl();
+    }
+
+    private void KeyCtrl()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            character.CharacterMove(Input.GetAxis("Horizontal"));
+        }
+    }
+}
